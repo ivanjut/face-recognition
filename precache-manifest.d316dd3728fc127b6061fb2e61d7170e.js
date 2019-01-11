d41fb1e67750a9f04a219b732526ba0c6097d53c
Ivan Jutamulia <ivanjutamulia@Ivans-MacBook-Pro.local>
2019-01-11 15:33:32 -0800

    Updates

---
+++ precache-manifest.d316dd3728fc127b6061fb2e61d7170e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff8a4ad9b5a6ad37d0182d9b8b89f2c9",
+    "url": "/face-recognition/static/media/brain.ff8a4ad9.png"
+  },
+  {
+    "revision": "00a7b039938598a9eb93",
+    "url": "/face-recognition/static/js/runtime~main.00a7b039.js"
+  },
+  {
+    "revision": "42e843482c41ca6657ff",
+    "url": "/face-recognition/static/js/main.42e84348.chunk.js"
+  },
+  {
+    "revision": "fe80fc5fa0b9cabfafa5",
+    "url": "/face-recognition/static/js/1.fe80fc5f.chunk.js"
+  },
+  {
+    "revision": "42e843482c41ca6657ff",
+    "url": "/face-recognition/static/css/main.cdbee86a.chunk.css"
+  },
+  {
+    "revision": "fe80fc5fa0b9cabfafa5",
+    "url": "/face-recognition/static/css/1.85504a70.chunk.css"
+  },
+  {
+    "revision": "7fd6949fd78e78c247300ccaffbbe841",
+    "url": "/face-recognition/index.html"
+  }
+];
